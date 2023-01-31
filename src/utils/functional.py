f7fbf0456ed40608ae4f1752b85cc42f74615c8c
DmitriyLT03 <lyutkind1@gmail.com>
2023-01-31 15:34:49 +0300

    Added src module and refactoring code

---
+++ src/utils/functional.py
@@ -0,0 +1,75 @@
+import torch
+
+
+def _take_channels(*xs, ignore_channels=None):
+    if ignore_channels is None:
+        return xs
+    else:
+        channels = [channel for channel in range(xs[0].shape[1]) if channel not in ignore_channels]
+        xs = [torch.index_select(x, dim=1, index=torch.tensor(channels).to(x.device)) for x in xs]
+        return xs
+
+
+def _threshold(x, threshold=None):
+    if threshold is not None:
+        return (x > threshold).type(x.dtype)
+    else:
+        return x
+
+
+def iou(pr, gt, eps=1e-7, threshold=None, ignore_channels=None):
+    pr = _threshold(pr, threshold=threshold)
+    pr, gt = _take_channels(pr, gt, ignore_channels=ignore_channels)
+
+    intersection = torch.sum(gt * pr)
+    union = torch.sum(gt) + torch.sum(pr) - intersection + eps
+    return (intersection + eps) / union
+
+
+jaccard = iou
+
+
+def f_score(pr, gt, beta=1, eps=1e-7, threshold=None, ignore_channels=None):
+    pr = _threshold(pr, threshold=threshold)
+    pr, gt = _take_channels(pr, gt, ignore_channels=ignore_channels)
+
+    tp = torch.sum(gt * pr)
+    fp = torch.sum(pr) - tp
+    fn = torch.sum(gt) - tp
+
+    score = ((1 + beta**2) * tp + eps) / ((1 + beta**2) * tp + beta**2 * fn + fp + eps)
+
+    return score
+
+
+def accuracy(pr, gt, threshold=0.5, ignore_channels=None):
+    pr = _threshold(pr, threshold=threshold)
+    pr, gt = _take_channels(pr, gt, ignore_channels=ignore_channels)
+
+    tp = torch.sum(gt == pr, dtype=pr.dtype)
+    score = tp / gt.view(-1).shape[0]
+    return score
+
+
+def precision(pr, gt, eps=1e-7, threshold=None, ignore_channels=None):
+    pr = _threshold(pr, threshold=threshold)
+    pr, gt = _take_channels(pr, gt, ignore_channels=ignore_channels)
+
+    tp = torch.sum(gt * pr)
+    fp = torch.sum(pr) - tp
+
+    score = (tp + eps) / (tp + fp + eps)
+
+    return score
+
+
+def recall(pr, gt, eps=1e-7, threshold=None, ignore_channels=None):
+    pr = _threshold(pr, threshold=threshold)
+    pr, gt = _take_channels(pr, gt, ignore_channels=ignore_channels)
+
+    tp = torch.sum(gt * pr)
+    fn = torch.sum(gt) - tp
+
+    score = (tp + eps) / (tp + fn + eps)
+
+    return score
