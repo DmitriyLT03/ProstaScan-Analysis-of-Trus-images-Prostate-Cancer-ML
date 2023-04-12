6e6434fca1dbe99e1a7da04047393327c007f505
DmitriyLT03 <lyutkind1@gmail.com>
2023-04-12 23:55:19 +0300

    Added: application with pytorch inference

---
+++ application/src/__init__.py
@@ -0,0 +1,39 @@
+from . import decoders
+
+from .decoders.unetplusplus import UnetPlusPlus
+from .decoders.fpn import FPN
+
+from typing import Optional as _Optional
+import torch as _torch
+
+
+def create_model(
+    arch: str,
+    encoder_name: str = "resnet34",
+    encoder_weights: _Optional[str] = "imagenet",
+    in_channels: int = 3,
+    classes: int = 1,
+    **kwargs,
+) -> _torch.nn.Module:
+
+    archs = [
+        FPN,
+        UnetPlusPlus,
+    ]
+    archs_dict = {a.__name__.lower(): a for a in archs}
+    try:
+        model_class = archs_dict[arch.lower()]
+    except KeyError:
+        raise KeyError(
+            "Wrong architecture type `{}`. Available options are: {}".format(
+                arch,
+                list(archs_dict.keys()),
+            )
+        )
+    return model_class(
+        encoder_name=encoder_name,
+        encoder_weights=encoder_weights,
+        in_channels=in_channels,
+        classes=classes,
+        **kwargs,
+    )
